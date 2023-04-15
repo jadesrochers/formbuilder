66760042bd155c6ddb57dbbfe65d3951b9f0cf79
jadesrochers <jadesrochers@gmail.com>
2023-04-15 12:44:14 -0400

    Fix all tests and demo setup

--- src/__tests__/formbuilder.test.js
+++ src/__tests__/formbuilder.test.js
@@ -1,209 +1,166 @@
 import React from 'react';
-import { mount } from '../enzyme';
+import { render, screen, renderHook, act, waitFor, fireEvent } from '@testing-library/react'
 import { Form, RegSelector, UpdateSelector, useSelector, useAddFormValue, OptionList } from '../formbuilder'
-import { act } from 'react-dom/test-utils';
-import { HookWrapper } from '@jadesrochers/reacthelpers'
+import '@testing-library/jest-dom'
 
 const FakeField = (props) => {
-  return(
-    <div />
-  )
+    return(
+        <div />
+    )
 }
 
 describe('formbuilder tests', () => {
-  test('Test useselector with sync data', () => {
-    let formset = jest.fn()
-    // Need to use mount for this hook to function
-    let wrapper = mount(<HookWrapper 
-      hook={() => useSelector([1,2,3], 2, formset, "noname")}   
-    >  
-    </HookWrapper>) 
-    wrapper.update()
-    /* console.log('useSelector: ', wrapper.debug()) */
-
-    let hook = wrapper.find('div').props().hook;
-    expect(hook.opts).toEqual([1,2,3])
-    expect(hook.current).toEqual(undefined)
-
-    /* hook = wrapper.find('div').props().hook; */
-    /* act(() => { */
-    /*   hook.setcurrent(3) */
-    /* }) */
-    /* wrapper.update() */
-    /* hook = wrapper.find('div').props().hook; */
-    /* expect(hook.current).toEqual(3) */
-
-  });
+    // Tests the useSelector Hook - sets option data, nothing else
+    test('Test useselector with sync data', async () => {
+        let formset = jest.fn()
+        const output = renderHook(() => useSelector([1,2,3], 2, formset, "noname"))
+        const { result } = output
+        expect(result.current.opts).toEqual([1,2,3])
+    });
 
 
-  // Fix this test when Async act() comes out and allows
-  // useEffect to work
-  test('Test useSelecor with async', async () => {
-    let dataProm = () => {
-      return new Promise((resolve, reject) => {
-      resolve([1,2,3,4,5]);
-     })
-    }
-    let testfn = jest.fn(() => [1,2,3,4])
-    let formset = jest.fn()
-    let wrapper 
-    await act( async () => {
-      wrapper = mount(
-        <HookWrapper 
-          hook={() => useSelector(testfn, 4, formset, "noname")}   
-        >
-        </HookWrapper>
-      ) 
-    })
+    // Fix this test when Async act() comes out and allows
+    // useEffect to work
+    test('Test useSelecor with async', async () => {
+        const dataProm = () => {
+            return new Promise((resolve, reject) => {
+                resolve([1,2,3,4,5]);
+            })
+        }
+        const testfn = () => [1,2,3,4]
+        const formset = jest.fn()
 
-    wrapper.update()
-    let hook = wrapper.find('div').props().hook;
-    expect(hook.opts).toEqual([1,2,3,4])
+        // Test a function returning the form data
+        let output = renderHook((props = {}) => useSelector(testfn, 4, formset, "noname"))
+        await waitFor(() => expect(output.result.current.opts).toEqual([1,2,3,4]))
 
-    await act( async () => {
-      wrapper = mount(
-        <HookWrapper 
-          hook={() => useSelector(dataProm, 4,  formset, "noname")}   
-        >
-        </HookWrapper>
-      ) 
-    })
-
-    wrapper.update()
-    hook = wrapper.find('div').props().hook;
-    expect(hook.opts).toEqual([1,2,3,4,5])
-  });
+        // Test an async function returning the form data
+        output = renderHook(() => useSelector(dataProm, 4, formset, "noname"))
+        await waitFor(() => expect(output.result.current.opts).toEqual([1,2,3,4,5]))
+    });
 })
 
+
 describe('OptionList tests', () => {
-  test('Get a set of options', () => {
-    let wrapper = mount(<OptionList options={[1,2,3,4]} />) 
-    // Tried an element match but did not work for unknown reason
-    // even though the options seem very simple.
-    expect(wrapper.find('option').get(0).props.item).toEqual(1)
-    expect(wrapper.find('option').get(1).props.item).toEqual(2)
-    expect(wrapper.find('option').get(2).props.item).toEqual(3)
-    expect(wrapper.find('option').get(3).props.item).toEqual(4)
-  });
+    test('Get a set of options', () => {
+        render(<OptionList options={[1,2,3,4]} />) 
+        expect(screen.getByText('1')).toBeInTheDocument()
+        expect(screen.getByText('2')).toBeInTheDocument()
+        expect(screen.getByText('3')).toBeInTheDocument()
+        expect(screen.getByText('4')).toBeInTheDocument()
+    });
 });
+
 
 describe('Selector tests', () => {
 
-  test('Create a selector', () => {
-    let formset = jest.fn()
-    let wrapper = mount(<RegSelector 
-      dataget={[1,2,3]} defaults={{"test1": undefined}}
-      varname="test1" formset={formset}
-      formvals={{'test1': 0}}
-      />) 
-    /* console.log(wrapper.debug()) */
-    expect(wrapper.find('select').props().value).toEqual(0)
-    expect(wrapper.text()).toEqual('123')
-  });
+    test('Create a selector', () => {
+        let formset = jest.fn()
+        const { container } = render(<RegSelector 
+            dataget={[1,2,3]} defaults={{"test1": undefined}}
+            varname="test1" formset={formset}
+            formvals={{'test1': 0}}
+            />) 
+        expect(container.getElementsByTagName('select').item(0)).toBeInTheDocument()
+        expect(screen.getByText('1')).toBeInTheDocument()
+        expect(screen.getByText('2')).toBeInTheDocument()
+        expect(screen.getByText('3')).toBeInTheDocument()
+    });
 
-  test('Change the value of a selector', () => {
-    let formset = jest.fn()
-    let wrapper = mount(<RegSelector 
-      dataget={[1,2,3]} defaults={{"test1": 999}}
-      varname="test1" formset={formset}
-      formvals={{'test1': 3}}
-      />) 
-    expect(wrapper.find('select').props().value).toEqual(3)
-    let onChange = wrapper.find('select').props().onChange
-    act(() => {
-      onChange({target: {value: 4}})
-    })
-    wrapper.update()
-    expect(wrapper.find('select').props().value).toEqual(3)
-    expect(wrapper.text()).toEqual('123')
-  });
+    test('Change the value of a selector', async () => {
+        const formset = jest.fn()
+        // Default text for selector is 999, should then update to [1 2 3]
+        const { container } = render(
+            <RegSelector 
+            dataget={[1,2,3]} defaults={{"test1": 999}}
+            varname="test1" formset={formset}
+            formvals={{'test1': 3}}
+            />) 
+        const selectElem = container.getElementsByTagName('select').item(0)
 
-  test('Create and Change an updateselector', async () => {
-    // Update when act() async support added, 16.9 goal for react team.
-    let formset = jest.fn()
-    let dataget = jest.fn()
-    let wrapper
-    dataget.mockReturnValueOnce([1,2,3])
-    dataget.mockReturnValueOnce([1,2,3])
-    act(() => {
-      wrapper = mount(<UpdateSelector
-         dataget={dataget} defaults={{'year': 2000}}
-         varname='year' formset={formset}
-         formvals={{'month': 1, 'year': 2001}}
-         changeon={['year', 'month']}
-         />)
-     })
-     expect(wrapper.find('select').props().value).toEqual(2001)
-     let onChange = wrapper.find('select').props().onChange
+        // Change the selected item
+        act(() => {
+            // You don't call Fcns with RTL, instead fire events
+            fireEvent.change(selectElem, {target: {value: 3}})
+        })
+        await waitFor(() => expect(screen.getByText('3')).toBeInTheDocument())
+    });
 
-     await act( async () => {
-       onChange({target: {value: 3}})
-     })
+    test('Create and Change an updateselector', async () => {
+        let formset = jest.fn()
+        let dataget = jest.fn()
+        let wrapper
+        dataget.mockReturnValueOnce([1,2,3])
+        dataget.mockReturnValueOnce([1,2,3])
+        const { container } = render(
+            <UpdateSelector
+            dataget={dataget} defaults={{'year': 2000}}
+            varname='year' formset={formset}
+            formvals={{'month': 1, 'year': 2001}}
+            changeon={['year', 'month']}
+            />)
+        const selectElem = container.getElementsByTagName('select').item(0)
+        expect(screen.getByText('2000')).toBeInTheDocument()
 
-     wrapper.update()
-     /* console.log(wrapper.debug()) */
-     // Now I don't know exactly what I intended, but 
-     // the value is kept stable unless there is clear cause to change it.
-     expect(wrapper.find('select').props().value).toEqual(2001)
-     expect(wrapper.text()).toEqual('123')
-  });
+        act(() => {
+            fireEvent.change(selectElem, {target: {value: 3}})
+        })
 
-
+        await waitFor(() => expect(screen.getByText('3')).toBeInTheDocument())
+        await waitFor(() => expect(screen.getByText('1')).toBeInTheDocument())
+        await waitFor(() => expect(screen.getByText('2')).toBeInTheDocument())
+    });
 
 });
 
 describe('Form tests', () => {
 
-  test('Create a form', () => {
-    let submitFormFcn = jest.fn()
-    let wrapper = mount(
-      <Form 
-        submitFormFcn={submitFormFcn}
-      >
-        <FakeField varname="test1" />
-      </Form> 
-      ) 
-    expect(wrapper.find('form').length).toEqual(1)
-  });
+    test('Create a form', () => {
+        let submitFormFcn = jest.fn()
+        const { container } = render(
+            <Form 
+            submitFormFcn={submitFormFcn}
+            >
+            <FakeField varname="test1" />
+            </Form> 
+        ) 
+        expect(container.getElementsByTagName('form').length).toEqual(1)
+    });
 
-  test('Create a form with field and submit it', () => {
-    let submitFormFcn = jest.fn()
-    let formset = jest.fn()
-    console.log('Currently broken test: \n')
-    let wrapper = mount(
-      <Form 
-        submitFormFcn={submitFormFcn}
-        defaults={{'test1': 1}}
-      >
-        <RegSelector
-          dataget={[1,2,3]}
-          varname='test1'  formset={formset}
-          formvals={{'test1': 0}}
-        />
-      </Form> 
-      ) 
-    let submit = wrapper.find('form').props().onSubmit 
-    submit({testsubmit: true})
-    expect(submitFormFcn).toHaveBeenCalledWith({"test1": 1}, {testsubmit: true})
-  });
+    test('Create a form with field and submit it', async () => {
+        let submitFormFcn = jest.fn()
+        let formset = jest.fn()
+        const { container } = render(
+            <Form 
+            submitFormFcn={submitFormFcn}
+            defaults={{'test1': 1}}
+            >
+            <RegSelector
+            dataget={[1,2,3]}
+            varname='test1'  formset={formset}
+            formvals={{'test1': 0}}
+            />
+            </Form> 
+        ) 
+
+        const inputElem = container.getElementsByTagName('input').item(0)
+        act(() => {
+            fireEvent.submit(inputElem)
+        })
+
+        await waitFor(() => expect(screen.getByText('3')).toBeInTheDocument())
+        expect(submitFormFcn).toHaveBeenCalledTimes(1)
+    });
 });
 
 describe('useAddFormHook tests', () => {
-  test('Create and set a value in the hook', () => {
-    let wrapper = mount(<HookWrapper 
-      hook={() => useAddFormValue()}   
-    >  
-    </HookWrapper>) 
+    test('Create and set a value in the hook', async () => {
+        const { result } = renderHook(() => useAddFormValue())
+        expect(result.current.values).toEqual({})
 
-    let hook = wrapper.find('div').props().hook;
-    expect(hook.values).toEqual({})
-    
-    act(() => {
-      hook.setname('test5', 61)
-    })
-    wrapper.update()
-    hook = wrapper.find('div').props().hook;
-
-    expect(hook.values).toEqual({test5: 61})
-  });
+        act(() => {
+            result.current.setname('test5', 61)
+        })
+        expect(result.current.values).toEqual({test5: 61})
+    });
 });
--- src/formbuilder.js
+++ src/formbuilder.js
@@ -5,83 +5,84 @@ import * as R from 'ramda';
 import * as fps from '@jadesrochers/fpstreamline';
 
 var handleChange = R.curry((name, formsetter, changed) => {
-  const value = changed.target.value;
-  /* console.log('handle change: ', name, value) */
-  formsetter(name, value)
+    const value = changed.target.value;
+    /* console.log('handle change: ', name, value) */
+    formsetter(name, value)
 })
 
-// This one only runs the useEffect once; good for static list of 
-// options or making request for single set that will not change.
+// Set the options for a List - Does Not select an option from that list
+// Runs the useEffect once; good for static list of 
+// options or making request for set that will not change later.
 const useSelector = (dataget, defaultval, formset, varname) => {
-  const [opts, setopts] = useState(defaultval)
-  /* const [current, setcurrent] = useState(defaultval) */
-  useEffect(()=> {
-  let datagetter
-    if(typeof dataget === "function"){
-      datagetter = async () => {
-        const data = await dataget()
-        setopts(data)
-        formset(varname, defaultval)
-        /* setcurrent(data[0]) */
-      }
-    }else{
-      datagetter = () => {
-        setopts(dataget)
-        formset(varname, defaultval)
-        /* setcurrent(dataget[0]) */
-      }
-    }
-    datagetter()
-  }, [])
+    const [opts, setopts] = useState(defaultval)
+    /* const [current, setcurrent] = useState(defaultval) */
+    useEffect(()=> {
+        let datagetter
+        if(typeof dataget === "function"){
+            datagetter = async () => {
+                const data = await dataget()
+                setopts(data)
+                formset(varname, defaultval)
+                /* setcurrent(data[0]) */
+            }
+        }else{
+            datagetter = () => {
+                setopts(dataget)
+                formset(varname, defaultval)
+                /* setcurrent(dataget[0]) */
+            }
+        }
+        datagetter()
+    }, [])
 
-  return { opts }
+    return { opts }
 }
 
 // If you need to pass static values, don't use this one.
 // Updates the options based on other values in the form
 const useUpdateSelector = ( props ) => {
-  const [opts, setopts] = useState(props.default)
-  const [rencount, setrencount] = useState(0)
-  /* const [current, setcurrent] = useState(props.default) */
-  const datawrap = () => props.dataget(props.formvals)
-  useEffect(()=> {
-    const datagetter = async () => {
-      const data = await datawrap()
-      /* console.log('data: ', data) */
-      // With multiple update selectors, need this to avoid
-      // invalid values
-      if(data){
-        /* setcurrent(data[0]) */
-        if(rencount === 0){
-          props.formset(props.varname, props.default)
-          setrencount(1)
-        }else if(rencount === 1){
-          props.formset(props.varname, data[0]) 
-          setrencount(2)
+    const [opts, setopts] = useState(props.default)
+    const [rencount, setrencount] = useState(0)
+    /* const [current, setcurrent] = useState(props.default) */
+    const datawrap = () => props.dataget(props.formvals)
+    useEffect(()=> {
+        const datagetter = async () => {
+            const data = await datawrap()
+            /* console.log('data: ', data) */
+            // With multiple update selectors, need this to avoid
+            // invalid values
+            if(data){
+                /* setcurrent(data[0]) */
+                if(rencount === 0){
+                    props.formset(props.varname, props.default)
+                    setrencount(1)
+                }else if(rencount === 1){
+                    props.formset(props.varname, data[0]) 
+                    setrencount(2)
+                }
+                setopts(data)
+            }
         }
-        setopts(data)
-      }
-    }
-    datagetter()
-  }, props.updatearr )
+        datagetter()
+    }, props.updatearr )
 
-  useEffect(()=> {
-    return () => {  
-      props.formset(props.varname, props.default)
-    };
-  }, [])
-  // The updatearr arg must be array, determines when the options are reset.
-  return { opts }
+    useEffect(()=> {
+        return () => {  
+            props.formset(props.varname, props.default)
+        };
+    }, [])
+    // The updatearr arg must be array, determines when the options are reset.
+    return { opts }
 }
 
 const useAddFormValue = () => {
-  const [values, setValues] = useState({})
-  const setname = (name, value) => {
-    let addon = {}
-    addon[name] = value
-    setValues({...values, ...addon})
-  }
-  return { values, setname, setValues }
+    const [values, setValues] = useState({})
+    const setname = (name, value) => {
+        let addon = {}
+        addon[name] = value
+        setValues({...values, ...addon})
+    }
+    return { values, setname, setValues }
 }
 
 const backgroundArrow = css`
@@ -119,7 +120,7 @@ const selectFilled = css`
   font-size: 1.0em;
 `;
 
-  /* background-color: #34495e; */
+/* background-color: #34495e; */
 const optionStyle = css`
   background-color: #124851;
   color: #d8d8d8;
@@ -129,46 +130,45 @@ const optionStyle = css`
 
 
 const OptionList = (props) => {
-  return(
-    R.map((opt) => (
-      <option 
-        key={fps.isTypeof('string')(opt) ? opt : R.toString(opt)} 
-        item={opt}
-        css={[(props.cssStyles ? props.cssStyles : undefined)]}
-      >
-        {opt} 
-      </option>
-    ))(fps.toArray(props.options))
-  )
+    return(
+        R.map((opt) => (
+            <option 
+            key={fps.isTypeof('string')(opt) ? opt : R.toString(opt)} 
+            item={opt}
+            css={[(props.cssStyles ? props.cssStyles : undefined)]}
+            >
+            {opt} 
+            </option>
+        ))(fps.toArray(props.options))
+    )
 }
 
 const Selector = (props) => {
- /* console.log('Selector name and formvals: ', props.formvals, props.varname) */
- const options = props.opts ? props.opts : ''
- return(
-   <select 
-    value = {props.formvals[props.varname]} 
-    key = {props.varname}
-    onChange = {(chg) => handleChange(props.varname, props.formset, chg)}
-    css={[selectFilled,backgroundArrow,hoverColor,(props.cssStyles ? props.cssStyles : undefined)]}
-    >
-     <OptionList
-       key={"formoptions"}
-       options={options}
-       cssStyles={props.optionStyle ? props.optionStyle : optionStyle}     />
-   </select>
- )
+    /* console.log('Selector name and formvals: ', props.formvals, props.varname) */
+    const options = props.opts ? props.opts : ''
+    return(
+        <select 
+        value = {props.formvals[props.varname]} 
+        key = {props.varname}
+        onChange = {(chg) => handleChange(props.varname, props.formset, chg)}
+        css={[selectFilled,backgroundArrow,hoverColor,(props.cssStyles ? props.cssStyles : undefined)]}
+        >
+        <OptionList
+        key={"formoptions"}
+        options={options}
+        cssStyles={props.optionStyle ? props.optionStyle : optionStyle}     />
+        </select>
+    )
 }
 
 const RegSelector = (props) => {
-console.log('RegSelector arbgs: ', props)
- const { opts } = useSelector(props.dataget, props.defaults[props.varname], props.formset, props.varname) 
- // Changed to useEffect to make React 6.13 happy regarding not updating
- // other components in fucntion body (with props.formset) except in useEffect
- useEffect(() => props.formset(props.varname, (props.defaults[props.varname])), [])
- /* useMemo(() => setcurrent(props.formvals[props.varname]), []) */
- return(
-   <Selector {...props}  opts={opts} />
+    const { opts } = useSelector(props.dataget, props.defaults[props.varname], props.formset, props.varname) 
+    // Changed to useEffect to make React 6.13 happy regarding not updating
+    // other components in fucntion body (with props.formset) except in useEffect
+    useEffect(() => props.formset(props.varname, (props.defaults[props.varname])), [])
+    /* useMemo(() => setcurrent(props.formvals[props.varname]), []) */
+    return(
+        <Selector {...props}  opts={opts} />
     )
 }
 
@@ -176,65 +176,65 @@ console.log('RegSelector arbgs: ', props)
 // Requires a getter function (dataget) to populate the options list, 
 // the state variable tracking the form values (formvals)
 const UpdateSelector = (props) => {
-  const updater = { ...props, updatearr: R.map((name) => props.formvals[name])(props.changeon), default: props.defaults[props.varname] } 
-  const { opts, } = useUpdateSelector(updater)
+    const updater = { ...props, updatearr: R.map((name) => props.formvals[name])(props.changeon), default: props.defaults[props.varname] } 
+    const { opts, } = useUpdateSelector(updater)
 
-  return(
-    <Selector {...props} opts={opts} />
-  )
+    return(
+        <Selector {...props} opts={opts} />
+    )
 }
 
 // The purpose of the nested selector: 
 // To allow updateselectors to depend on each other in a certain order
 // so that they will always get the correct result
 const NestedSelector = (props) => {
-  const updater = { ...props, updatearr: R.map((name) => props.formvals[name])(props.changeon), default: props.defaults[props.varname] } 
-  const { opts, current, setcurrent } = useUpdateSelector(updater)
+    const updater = { ...props, updatearr: R.map((name) => props.formvals[name])(props.changeon), default: props.defaults[props.varname] } 
+    const { opts, current, setcurrent } = useUpdateSelector(updater)
 
-  const pass = { formvals: props.formvals, formset: props.formset, defaults: props.defaults }
-  const propsToChildren = R.map(child => {
-    return React.cloneElement(child, { ...pass })
-  })(fps.toArray(props.children))
+    const pass = { formvals: props.formvals, formset: props.formset, defaults: props.defaults }
+    const propsToChildren = R.map(child => {
+        return React.cloneElement(child, { ...pass })
+    })(fps.toArray(props.children))
 
- // The useMemo statement assumes that the value of this selector
- // is needed for the nested selector to update. If not, don't use this.
- return(
-   <>
-     <Selector {...props} opts={opts} setcurrent={setcurrent} current={current} />
-     { propsToChildren }
-   </>
- )
-     /* { useMemo(() => propsToChildren, R.values(R.pick(props.nestchange, props.formvals)) )} */
+    // The useMemo statement assumes that the value of this selector
+    // is needed for the nested selector to update. If not, don't use this.
+    return(
+        <>
+        <Selector {...props} opts={opts} setcurrent={setcurrent} current={current} />
+        { propsToChildren }
+        </>
+    )
+    /* { useMemo(() => propsToChildren, R.values(R.pick(props.nestchange, props.formvals)) )} */
 }
 
 
 // Need TO DO: have Form set all the defaults itself to avoid unset 
 // value problems
 const Form = (props) => {
-  const { values, setname, setValues } = useAddFormValue()
-  const pass = {formset: setname, formvals: values, defaults: props.defaults, cssStyles: props.cssStyles, optionStyle: props.optionStyle}
-  const propsToChildren = R.map(child => {
-    return React.cloneElement(child, { ...pass, key: child.props.varname})
-  })(fps.toArray(props.children))
- 
-  if( R.isEmpty(values) ){
-    setValues(props.defaults)
-  }
-  
-  // The props.submitFcn is usually to get the data back to the parent.
-  return(
-  <form 
-     onSubmit={(e) => {
-       props.submitFormFcn(values,e) 
-     } }
-  >
-    { propsToChildren }
-    <input css={[ selectFilled, colorBorder, hoverColor, activeState,(props.submitStyle ? props.submitStyle : props.cssStyles)]}
-     type={"submit"} value={"Submit"}
-     key={"submitbutton"}
-    />
-  </form>
-  )
+    const { values, setname, setValues } = useAddFormValue()
+    const pass = {formset: setname, formvals: values, defaults: props.defaults, cssStyles: props.cssStyles, optionStyle: props.optionStyle}
+    const propsToChildren = R.map(child => {
+        return React.cloneElement(child, { ...pass, key: child.props.varname})
+    })(fps.toArray(props.children))
+
+    if( R.isEmpty(values) ){
+        setValues(props.defaults)
+    }
+
+    // The props.submitFcn is usually to get the data back to the parent.
+    return(
+        <form 
+        onSubmit={(e) => {
+            props.submitFormFcn(values,e) 
+        } }
+        >
+        { propsToChildren }
+        <input css={[ selectFilled, colorBorder, hoverColor, activeState,(props.submitStyle ? props.submitStyle : props.cssStyles)]}
+        type={"submit"} value={"Submit"}
+        key={"submitbutton"}
+        />
+        </form>
+    )
 }
 
 export { Form, RegSelector, UpdateSelector, NestedSelector, useSelector, useAddFormValue, OptionList }
